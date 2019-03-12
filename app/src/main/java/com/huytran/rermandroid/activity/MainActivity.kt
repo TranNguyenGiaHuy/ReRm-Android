ff1043660991dcbbed066b0a79a91094b6dfbccd
Nguyen Hoang My <15520500@gm.uit.edu.com>
2019-03-12 22:10:59 +0700

    Profile tab

--- app/src/main/java/com/huytran/rermandroid/activity/MainActivity.kt
+++ app/src/main/java/com/huytran/rermandroid/activity/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.huytran.rermandroid.activity
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import com.huytran.rermandroid.R
 import com.huytran.rermandroid.activity.base.BaseActivity
+import com.huytran.rermandroid.fragment.ExploreFragment
+import com.huytran.rermandroid.fragment.ProfileFragment
+import com.huytran.rermandroid.fragment.TestFragment
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_profile -> {
+                val profileFragment = ProfileFragment.newInstance()
+                openFragment(profileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_explore -> {
+                val exploreFragment = ExploreFragment.newInstance()
+                openFragment(exploreFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
