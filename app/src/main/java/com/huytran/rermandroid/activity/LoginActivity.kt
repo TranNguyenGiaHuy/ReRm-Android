bc2c054b2436bf113d8320feb966bab4f5c6e33b
Huy Tran <15520318@gm.uit.edu.vn>
2019-04-08 09:02:12 +0700

    fix merge

--- app/src/main/java/com/huytran/rermandroid/activity/LoginActivity.kt
+++ app/src/main/java/com/huytran/rermandroid/activity/LoginActivity.kt
@@ -1,14 +1,11 @@
 package com.huytran.rermandroid.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.View
-import android.widget.Toast
 import com.huytran.rermandroid.R
 import com.huytran.rermandroid.activity.base.BaseActivity
 import com.huytran.rermandroid.fragment.LoginFragment
 import com.huytran.rermandroid.manager.TransactionManager
+import javax.inject.Inject
 
 class LoginActivity @Inject constructor() : BaseActivity() {
 
